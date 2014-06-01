71faf4539dfff4954cadbda3ef3d8344960fc77d
azarateo <azarate@cool4code.com>
2014-06-01 06:03:00 -0500

    detalle

--- supermercacom/feedbackDetailViewController.m
+++ supermercacom/feedbackDetailViewController.m
@@ -30,26 +30,12 @@
 @synthesize labelCalifTrans;
 
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
     [self actualizarUI];
     // Do any additional setup after loading the view.
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 -(void) actualizarUI{
